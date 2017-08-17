859a3c2035e30b5c87018a8ab86c16f42a593047
Renz Ramos <im.renzramos@gmail.com>
2017-08-17 15:16:31 +0800

    Create jquery-sticky-side.1.0.js

---
+++ jquery-sticky-side.1.0.js
@@ -0,0 +1,90 @@
+/*
+Name: Sticky Side
+Description: Make the sidebar floatin
+Version: 1.0
+Author: Renz Ricalde Ramos
+Author URL: http://www.renzramos.com
+Tags: sticky,sidebar,stick,side
+*/
+(function ( $ ) {
+ 
+    $.fn.stickyRose = function( options ) {
+        
+        // default
+        var settings = $.extend({
+            gapTop: 15,
+            gapBottom: 15,
+            selector: '.sticky-sidebar',
+            responsive: {
+				'min': 768,
+				'max': 9999,
+			}
+        }, options );
+
+        // declarations
+        var win = $(window);
+		var stickSidebar =  $(settings.selector) ;
+		var stickSidebarHeight =  stickSidebar.outerHeight();
+		var stickSidebarParentWidth = stickSidebar.closest('div').width();
+
+		var mainContainerOffset = $(this).offset().top;
+		var mainContainerHeight = $(this).outerHeight();
+		var mainContainerBottomOffset = (mainContainerOffset + mainContainerHeight) - stickSidebarHeight;
+
+		// styles
+		var styles = {
+			'relative': {
+				"position": "relative", 
+				"top": 'auto',
+				"width": 'auto',
+				"bottom": "auto"
+			},
+        	'fixed': {
+				"position": "fixed", 
+				"top": settings.gapTop,
+				"width": stickSidebarParentWidth,
+				"bottom": "auto"
+			},
+			'absolute': {
+				"position": "absolute", 
+				"top": "auto",
+				"width": stickSidebarParentWidth,
+				"bottom": settings.gapBottom
+			}
+        };
+
+        // initialize
+
+		initStickyFloating();
+
+		win.scroll(function(){
+			initStickyFloating();
+		});
+
+		win.resize(function(){
+			initStickyFloating();
+		});
+
+		function initStickyFloating(){
+			if (win.width() >= settings.responsive.min && win.width() <= settings.responsive.max ){
+				var scrollTop = win.scrollTop();
+
+				if (scrollTop >= mainContainerOffset - settings.gapTop){
+
+					if (scrollTop < mainContainerBottomOffset){
+						stickSidebar.css(styles.fixed);
+					}else{
+						stickSidebar.css(styles.absolute);
+					}
+				}else{
+					stickSidebar.css(styles.relative);
+				}
+				console.log('Sticky Side 1.0 - Running');
+			}else{
+				stickSidebar.css(styles.relative);
+				console.log('Sticky Side 1.0 - Stop');
+			}
+		}
+    };
+ 
+}( jQuery ));
